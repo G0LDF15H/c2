62d6b2aa3103c863c07e7c61b06a6e1350ebe2b6
Rebecca Huang <rebecca.huang@utexas.edu>
2024-03-05 14:47:26 -0600

    huh

--- test_client.py
+++ test_client.py
@@ -1,5 +1,5 @@
 import socket
 
 clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientsocket.connect(('10.0.2.4', 8089))
+clientsocket.connect(('10.0.2.4', 80))
 clientsocket.send('hello')
--- test_server.py
+++ test_server.py
@@ -1,7 +1,7 @@
 import socket
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serversocket.bind(('localhost', 8089))
+serversocket.bind(('127.0.0.1', 80))
 serversocket.listen(5) # become a server socket, maximum 5 connections
 
 while True:
