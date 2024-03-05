5782410f8fa07d696deb1c59308157ea317adc0b
dannydktr <93729067+dannydktr@users.noreply.github.com>
2024-03-05 14:29:13 -0600

    client

--- client.py
+++ client.py
@@ -1,22 +1,23 @@
 import socket
 
 HOST = "10.0.2.4"
-PORT = 80
+PORT = 65432
 # client
 def main():
     # HOST = raw_input("give me a host to connect to :3: ") # for python 2.7.5
     print("running client side with: " + HOST + " and " + str(PORT))
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((HOST, PORT))
 
-    # send something
-    while True: 
-        something = input("what do you wnant to send to this not evil thingy (enter Q to exit): ")
-        if something == "Q":
-            break
-        client.send(something.encode())
-        
-    client.close()
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+        client.connect((HOST, PORT))
+
+        # send something
+        while True: 
+            something = input("what do you wnant to send to this not evil thingy (enter to exit): ")
+            if something == "":
+                break
+            client.send(something.encode())
+            
+        client.close()
 
 if __name__ == "__main__":
     main()
